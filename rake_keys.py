9c40710bc894e27cd933c0e991421d0999e3d830
Jerome Conrads <jerome.conrads@gmx.de>
2023-02-20 19:13:34 +0100

    txt

--- rake_keys.py
+++ rake_keys.py
@@ -12,17 +12,14 @@ from rake_nltk import Rake
 
 data_loc ="/media/jerome/Bacheler/Gutenbergfiles/"
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+corpus = [
+     'This is the first document.',
+     'This document is the second document.',
+     'And this is the third one.',
+ ]
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(corpus)
+print(vectorizer.get_feature_names_out())
 
-count_location = path.join(data_loc,"data/text/")
-count_names = np.array(listdir(count_location))
-
-location = path.join(count_location,count_names[400])
-print(location)
-txt = pd.read_csv(location, header=None,sep=".",encoding = "utf-8",on_bad_lines='skip')
-print(txt.values)
-print(count_names[1])
-#txt = txt.words.str.cat(sep=' ')
-r = Rake()
-r.extract_keywords_from_sentences(txt)
-r.get_ranked_phrases()
-print(r.get_ranked_phrases()[:20])
+print(X)
